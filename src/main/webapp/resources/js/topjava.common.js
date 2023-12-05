5ebdbdb0add62c6e78d69b3680043dcb89178e74
Alexander Savelev <alex.savelev21@yandex.ru>
2023-12-05 22:25:02 +0500

    HW_8 add meals.js, meals with Bootstrap

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -50,31 +50,31 @@ function save() {
     });
 }
 
-let failedNote;
+    let failedNote;
 
-function closeNoty() {
-    if (failedNote) {
-        failedNote.close();
-        failedNote = undefined;
+    function closeNoty() {
+        if (failedNote) {
+            failedNote.close();
+            failedNote = undefined;
+        }
     }
-}
 
-function successNoty(text) {
-    closeNoty();
-    new Noty({
-        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
-        type: 'success',
-        layout: "bottomRight",
-        timeout: 1000
-    }).show();
-}
+    function successNoty(text) {
+        closeNoty();
+        new Noty({
+            text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
+            type: 'success',
+            layout: "bottomRight",
+            timeout: 1000
+        }).show();
+    }
 
-function failNoty(jqXHR) {
-    closeNoty();
-    failedNote = new Noty({
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
-        type: "error",
-        layout: "bottomRight"
-    });
-    failedNote.show()
+    function failNoty(jqXHR) {
+        closeNoty();
+        failedNote = new Noty({
+            text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
+            type: "error",
+            layout: "bottomRight"
+        });
+        failedNote.show()
 }
